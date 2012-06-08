b788ded3a3aefa1fce4a228d71d7e883d9410a01
Scott Fleckenstein <nullstyle@gmail.com>
2012-06-08 14:35:28 -0700

    Add system based rng

--- lib/randy/rng/system.rb
+++ lib/randy/rng/system.rb
@@ -3,10 +3,13 @@ module Randy::Rng
   # Uses ruby's built in Random class to generate the numbers
   #
   class System < Base
-    I_LIMIT = (2**32 - 1)
-    I_MINUS = 2**31
-    L_LIMIT = (2**64 - 1)
-    L_MINUS = 2**63
+    MAX_I = 2**31 - 1
+    MIN_I = -(2**31)
+    I_LIMIT = MIN_I..MAX_I
+
+    MAX_L = 2**63 - 1
+    MIN_L = -(2**63)
+    L_LIMIT = MIN_L..MAX_L
 
     def initialize(*args)
       super *args
@@ -14,11 +17,11 @@ module Randy::Rng
     end
 
     def next_i
-      @rand.rand(I_LIMIT) - I_MINUS
+      @rand.rand(I_LIMIT)
     end
     
     def next_l
-      @rand.rand(L_LIMIT) - L_MINUS
+      @rand.rand(L_LIMIT)
     end
 
     def next_f
